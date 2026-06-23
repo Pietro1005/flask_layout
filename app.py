3e12767595cd391c8e57f3638e79fe906ba4070b
Pietro1005 <pietrocorreali@colegiopaulodetarso.com.br>
2026-06-23 14:20:56 +0000

    sdg

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template('index.html')
+def index():]
+    aluno = {
+        "nome": "Kinque"
+        "turma": "2° Ensino Medio Técnico"
+    }
+    return render_template('index.html', title="home" aluno=aluno)
 
 @app.route("/boletim")
 def boletim():
-    return render_template('boletim.html')
+    return render_template('boletim.html', title="boletim")
