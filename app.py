0839dfabc21304da91a7dc3c482abe2107fed6f6
Pietro1005 <pietrocorreali@colegiopaulodetarso.com.br>
2026-08-04 10:21:25 +0000

    kkkkk

--- app.py
+++ app.py
@@ -3,20 +3,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():]
+def index():
     aluno = {
         "nome": "Kinque"
-        "turma": "2° Ensino Medio Técnico"
+        "turma:" "2° Ensino Medio Técnico"
     }
     professores = [
         {
             "nome" = "Felipe Ishara",
-            "materia": "Web II"
+            "materia:" "Web II"
         },
 
         {
             "nome" = "Edidio Lima"
-            "materia" = "Software"
+            "materia:" = "Software"
         }
     ]
     return render_template('index.html', title="home" aluno=aluno, professores=professores )
